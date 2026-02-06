7f8231eeb606bba5973fdf72bdbdb85ec7ef92f3
Ait Cherif M'hand <ait-cherif.mhand@outlook.com>
2026-02-06 14:46:07 +0100

    Website update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     const navLinks = document.querySelectorAll('nav ul li a');
     navLinks.forEach(link => {
         link.addEventListener('mouseover', () => {
@@ -7,5 +8,16 @@ document.addEventListener("DOMContentLoaded", function () {
         link.addEventListener('mouseout', () => {
             link.style.transform = "scale(1)";
         });
+    });
+
+    const langBtn = document.getElementById('langToggle');
+    const htmlTag = document.documentElement;
+
+    langBtn.addEventListener('click', () => {
+        if (htmlTag.getAttribute('lang') === 'fr') {
+            htmlTag.setAttribute('lang', 'en');
+        } else {
+            htmlTag.setAttribute('lang', 'fr');
+        }
     });
 });
